fee3ded4e09eae9e2cebe2a773820e1e98aeeb2c
Alexey <mr-game.gorbunoff@yandex.ru>
2019-01-20 17:27:15 +0300

    add makefiles

--- GKLStest/example.cpp
+++ GKLStest/example.cpp
@@ -1,5 +1,4 @@
-#include <cstdio>
-#include <cstdlib>
+#include <cmath>
 #include <chrono>
 #include <iostream>
 #include <iomanip>
--- GrishaginTest/vagris.c
+++ GrishaginTest/vagris.c
@@ -14,6 +14,7 @@
 
 
 #include <math.h>
+#include <stdlib.h>
 #include "vagris.h"
 
 double rand_minimums[]={
