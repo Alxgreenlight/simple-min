92fb7d75f37d9c449b1c65473d9b74f7fbc9ac40
Alexey <46227043+Alxgreenlight@users.noreply.github.com>
2019-10-06 14:28:12 +0300

    OneDim temp realization added

---
+++ OneDim_temp/OneDimStarter.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <fstream>
+#include "../solver/R_Optim.hpp"
+#include "miniOneDimFuncs.hpp"
+#include "../solver/UltraOneDim.hpp"
+
+std::chrono::steady_clock sc;
+std::chrono::milliseconds atime_span = std::chrono::duration_values<std::chrono::milliseconds>::zero();
+onedimopt::BasicFunc<double> *ptr;
+
+double func(const double* x) {	/* wrapper for function providing to solver */
+	return ptr->calculate(x[0]);
+}
+
+int main()
+{
+	std::ofstream out;
+	out.open("Results.txt", std::ios::out);
+	onedimopt::BMsOneDim<double> bms;
+	double UpperBound;
+	int nodes;
+	double eps;
+	unsigned long long int fevals;
+	unsigned long int iters;
+	double xfound;
+
+	std::cout << "Set number of nodes per dimension" << std::endl << \
+		"It can significantly affect on performance!" << std::endl;
+	std::cin >> nodes;
+	while (std::cin.fail()) {
+		std::cerr << "Please, repeat input" << std::endl;
+		std::cin >> nodes;
+	}
+
+	std::cout << "Set accuracy" << std::endl << "It can significantly affect on performance!" << std::endl;
+	std::cin >> eps;
+	while (std::cin.fail()) {
+		std::cerr << "Please, repeat input" << std::endl;
+		std::cin >> eps;
+	}
+	for (auto elem : bms) {
+		ptr = &(*elem);
+		std::cout << elem->detDesc() << std::endl;
+		std::cout << "With bounds [" << elem->LBound() << "," << elem->RBound() << "]" << std::endl;
+		std::cout << "In x = " << elem->RealminX() <<  " and real y = " << elem->RealminY() << std::endl;
+		try {
+			rOptimizer<double> rOpt;
+			rOpt.init(1, nodes, eps);
+			auto start = sc.now();
+			double lb = elem->LBound(), rb = elem->RBound();
+			UpperBound = rOpt.search(1, &xfound, &lb, &rb, func);
+			auto end = sc.now();
+			auto time_span = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+			double diff = 1.0*fabs(elem->RealminY() - UpperBound);
+			rOpt.getInfo(fevals, iters);
+			std::cout << std::fixed << "Search complete:" << std::endl << "Upper bound: " << std::setprecision(10) << UpperBound << std::endl << \
+				"At " << xfound << ", with L = " << rOpt.maxL << std::endl;
+			std::cout << std::fixed << "Diff: " << std::setprecision(10) << diff << std::endl;
+			std::cout << "Function evaluations: " << fevals << " in " << iters << " iterations" << std::endl;
+			std::cout << "Evaluation time: " << time_span.count() << "ms" << std::endl;
+			rOpt.clear();
+			double L = ultraoptimizer<double>(100000000, lb, rb, xfound, UpperBound, func);
+			std::cout << "Very accurate estimation: L = " << L << ", Upper bound = " << UpperBound << ", at " << xfound << std::endl;
+		}
+		catch (std::exception& e) {
+			std::cout << e.what() << std::endl;
+			out.close();
+			return -1;
+		}
+		std::cout << std::endl << std::endl;
+	}
+	out.close();
+	return 0;
+}
+
+
---
+++ OneDim_temp/miniOneDimFuncs.hpp
@@ -0,0 +1,147 @@
+#ifndef MINIONEDIMFUNCS_HPP
+#define MINIONEDIMFUNCS_HPP
+
+
+#include <cmath>
+#include <vector>
+#include <string>
+
+# define MY_PI          3.141592653589793238462643383279502884L /* pi */
+
+/*
+
+All functions from http://infinity77.net/global_optimization/test_functions_1d.html
+
+*/
+
+namespace onedimopt {
+	template <class T>
+	class BasicFunc {
+	protected:
+		T RealX, RealY;
+		T LB;
+		T RB;
+		std::string desc;
+	public:
+		BasicFunc(T l = -1.0, T r = 1.0) :LB(l), RB(r) {}
+		virtual T calculate(const T coord) = 0;
+		virtual std::string detDesc() const {
+			return desc;
+		}
+		T RealminX() {
+			return RealX;
+		}
+		T RealminY() {
+			return RealY;
+		}
+		T LBound() {
+			return LB;
+		}
+		T RBound() {
+			return RB;
+		}
+	};
+	template <class T>
+	class Func1 : public BasicFunc<T> {
+	public:
+		Func1(const T l = 2.7, const T r = 7.5) :BasicFunc<T>(l, r) {
+			this->RealX = 5.145735;
+			this->RealY = -1.899599;
+			this->desc = "Problem 02";
+		}
+		virtual T calculate(const T coord) {
+			return sin(coord) + sin(10.0 / 3.0*coord);
+		}
+	};
+	template <class T>
+	class Func2 : public BasicFunc<T> {
+	public:
+		Func2(const T l = 1.9, const T r = 3.9) :BasicFunc<T>(l, r) {
+			this->RealX = 2.868034;
+			this->RealY = -3.85045;
+			this->desc = "Problem 04";
+		}
+		virtual T calculate(const T coord) {
+			return -1.0*exp(-1.0*coord)*(16.0*coord*coord - 24 * coord + 5);
+		}
+	};
+	template <class T>
+	class Func3 : public BasicFunc<T> {
+	public:
+		Func3(const T l = 0.0, const T r = 1.2) :BasicFunc<T>(l, r) {
+			this->RealX = 0.96609;
+			this->RealY = -1.48907;
+			this->desc = "Problem 05";
+		}
+		virtual T calculate(const T coord) {
+			return -1.0*sin(18.0*coord)*(1.4 - 3 * coord);
+		}
+	};
+
+	template <class T>
+	class Func4 : public BasicFunc<T> {
+	public:
+		Func4(const T l = 2.7, const T r = 7.5) :BasicFunc<T>(l, r) {
+			this->RealX = 5.19978;
+			this->RealY = -1.6013;
+			this->desc = "Problem 07";
+		}
+		virtual T calculate(const T coord) {
+			return sin(coord) + sin(10.0 / 3.0*coord) + log(coord) - 0.84*coord + 3;
+		}
+	};
+
+	template <class T>
+	class Func5 : public BasicFunc<T> {
+	public:
+		Func5(const T l = 0.0, const T r = 4.0) :BasicFunc<T>(l, r) {
+			this->RealX = 0.224885;
+			this->RealY = -0.788685;
+			this->desc = "Problem 14";
+		}
+		virtual T calculate(const T coord) {
+			return -1.0*exp(-1.0*coord)*sin(2 * MY_PI*coord);
+		}
+	};
+
+	template <class T>
+	class BMsOneDim {
+		std::vector<std::shared_ptr<BasicFunc<T>>> v;
+		void add(const std::shared_ptr<BasicFunc<T>> &ptr)
+		{
+			v.push_back(ptr);
+		}
+	public:
+		BMsOneDim() {
+			add(std::make_shared<Func1<double>>());
+			add(std::make_shared<Func2<double>>());
+			add(std::make_shared<Func3<double>>());
+			add(std::make_shared<Func4<double>>());
+			add(std::make_shared<Func5<double>>());
+
+		}
+		~BMsOneDim() {
+			v.clear();
+		}
+		typename std::vector<std::shared_ptr<BasicFunc<T>>>::iterator begin()
+		{
+			return v.begin();
+		}
+		typename std::vector<std::shared_ptr<BasicFunc<T>>>::iterator end()
+		{
+			return v.end();
+		}
+		typename std::vector<std::shared_ptr<BasicFunc<T>>>::const_iterator begin() const
+		{
+			return v.begin();
+		}
+		typename std::vector<std::shared_ptr<BasicFunc<T>>>::const_iterator end() const
+		{
+			return v.end();
+		}
+	};
+
+
+}
+
+#endif
